45ab2fba75f5d0186b85a4ab2225e515cc38c670
Gian Ortiz <gianortiz@tutanota.com>
2018-08-27 13:31:36 -0300

    Now running with react-native instead of create-react-native-app

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import MainMenu from './screens/MainMenu.js';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => MainMenu);
