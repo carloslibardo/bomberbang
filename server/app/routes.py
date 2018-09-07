0724167cfed7f16c8b92b35171c4578c4a5891c1
Gian Ortiz <brksgian@gmail.com>
2018-09-07 19:07:48 +0000

    Adding server logic

---
+++ server/app/routes.py
@@ -0,0 +1,29 @@
+from app import app, db, login
+from flask import jsonify, request
+from flask_login import current_user, logout_user, login_user, login_required
+from app.models import User
+
+@app.route('/app/login', methods=['GET'])
+def logger():
+	if not request.json:
+		#handle error
+		json_return = [ { 'logged': False, 'cause': 'miss request arguments' } ]
+		return jsonify({'resp': json_return})
+	elif current_user.is_authenticated:
+		#handle
+		json_return = [ { 'logged': True, 'cause': 'user is already logged' } ]
+		return jsonify({'resp': json_return})
+	else:
+		usr = request.json['user']
+		password = request.json['password']
+		user = User.query.filter_by(username=usr).first()
+		if user is None or not user.check_password(password):
+			#handle login error(username or password are incorrect)
+			json_return = [ { 'logged': False, 'cause': 'password or user incorrect' } ]
+			return jsonify({'resp': json_return})
+		login_user(user, remember=True)
+		json_return = [ { 'logged': True, 'cause': 'user data are correct' } ]
+		return jsonify({'resp': json_return})
+	json_return = [ { 'logged': 'Unknown', 'cause': 'not handled request' } ]
+	return jsonify({'resp': json_return})
+
