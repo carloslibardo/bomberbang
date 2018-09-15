4397b102b29f92e04be6704db2d863f43fa50670
kaduelinho <carlos.libardo@chaordicsystems.com>
2018-09-15 14:27:38 -0300

    Recreate login screen

--- app/App.js
+++ app/App.js
@@ -1,36 +1,60 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-  TextInput, 
   Text,
-  View} from 'react-native';
+  View,
+  Image
+} from 'react-native';
 import Login from '../components/loginModal';
+import LoginFooter from '../components/loginFooter';
+import { createStore, bindActionCreators } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  modalVisible: false
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ENABLE_MODAL':
+      return { modalVisible: true }
+  }
+
+  return state;
+}
+
+const store = createStore(reducer);
 
 type Props = {};
-export default class App extends Component{
+export default class App extends Component {
+  state = {
+    modalVisible: false,
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <View >
         <View style={styles.header}>
-          <Text style = {styles.headerText}>Começo de dois garotos milhonarios</Text>
+          <Text style={styles.headerText}>Começo de dois garotos milhonarios</Text>
         </View>
-        <Login></Login>
+        <Image
+          style={{ width: 360, height: 400 }}
+          source={require('../components/pp.jpeg')}
+        >
+        </Image>
+        <Provider store={store}>
+          <LoginFooter></LoginFooter>
+        </Provider>
+        <Login visible={this.state.modalVisible}></Login>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-  },
   header: {
     height: (Platform.OS === 'ios') ? 70 : 50,
-    paddingTop: (Platform.OS === 'ios') ? 20: 0,
+    paddingTop: (Platform.OS === 'ios') ? 20 : 0,
     backgroundColor: '#FFF',
     justifyContent: 'center',
     alignItems: 'center',
@@ -39,4 +63,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold'
   },
+
 });
--- components/loginModal.js
+++ components/loginModal.js
@@ -1,17 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     View,
     Text,
     Modal,
     TextInput,
     TouchableOpacity,
-    StyleSheet
+    StyleSheet,
+    Alert
 } from 'react-native';
 
 export default class LoginModal extends Component {
-    render(){
+
+    render() {
         return (
-            <Modal animationType="fade" transparent={true} visible = {true}>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={this.props.visible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed');
+                }}
+            >
                 <View style={styles.modalContainer}>
                     <View style={styles.boxContainer}>
                         <Text style={styles.boxTitle}>Faça seu login</Text>
@@ -23,28 +32,11 @@ export default class LoginModal extends Component {
                         >
                         </TextInput>
                         <TextInput
-                            autoFocus
                             autoCapitalize="none"
                             style={styles.boxInput}
                             placeholder="Digite sua senha"
                         >
                         </TextInput>
-
-                        <View style={styles.buttonContainer}>
-                            <TouchableOpacity
-                                style={[styles.button, styles.registerButton]}
-                                onPress ={() => {}}
-                            >
-                                <Text style = {styles.buttonText}>Registrar</Text>
-                            </TouchableOpacity>
-
-                            <TouchableOpacity
-                                style={[styles.button, styles.loginButton]}
-                                onPress ={() => {}}
-                            >
-                                <Text style = {styles.buttonText}>Entrar</Text>
-                            </TouchableOpacity>
-                        </View>
                     </View>
                 </View>
             </Modal>
@@ -53,56 +45,32 @@ export default class LoginModal extends Component {
 }
 
 const styles = StyleSheet.create({
-   modalContainer: {
-       flex: 1,
-       backgroundColor: 'rgba(0,0,0, 0.1)',
-       justifyContent: 'center' ,
-       alignItems: 'center',
-   },
-   boxContainer: {
-       padding: 20,
-       backgroundColor: '#FFF',
-       borderRadius: 10,
-       alignItems: 'center',
-       width: 280,
-       height: 200
-   },
-   boxTitle: {
-       fontWeight: 'bold',
-       fontSize: 16,
-   },
-   boxInput: {
-       alignSelf: 'stretch',
-       marginTop: 10,
-       paddingVertical: 0,
-       paddingHorizontal: 20,
-       borderWidth: 1,
-       borderColor: '#DDD',
-       height: 40,
-       borderRadius: 3
-   },
-   buttonContainer: {
-     marginTop: 10,
-     height: 40,
-     flexDirection: 'row'  
-   },
-   button: {
-       flex: 1,
-       alignItems: 'center',
-       justifyContent: 'center',
-       borderRadius: 3,
-   },
-   registerButton: {
-    backgroundColor: '#E25F5F',
-    marginRight: 5,
-   },
-   loginButton: {
-       backgroundColor: '#70BD85',
-       marginLeft: 5,
-   },
-   buttonText:{
-       fontWeight:'bold',
-       color: "#FFF",
-       fontSize: 12,
-   }
+    modalContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0, 0.7)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    boxContainer: {
+        padding: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        alignItems: 'center',
+        width: 280,
+        height: 200
+    },
+    boxTitle: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    boxInput: {
+        alignSelf: 'stretch',
+        marginTop: 10,
+        paddingVertical: 0,
+        paddingHorizontal: 20,
+        borderWidth: 1,
+        borderColor: '#DDD',
+        height: 40,
+        borderRadius: 3
+    },
 })
